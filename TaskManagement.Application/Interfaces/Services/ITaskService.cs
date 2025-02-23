2412d27088d0b85db372e230669e3207e5ead6ab
Thiago Figueira <thi.figueira@hotmail.com>
2025-02-22 22:07:36 -0300

    feat: adjust task service

---
+++ TaskManagement.Application/Interfaces/Services/ITaskService.cs
@@ -0,0 +1,18 @@
+﻿using TaskManagement.Application.DTOs;
+using TaskManagement.Domain.Enums;
+
+namespace TaskManagement.Application.Interfaces.Services
+{
+    /// <summary>
+    /// Interface do serviço de tarefas.
+    /// </summary>
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskDto>> GetAllTasksAsync();
+        Task<IEnumerable<TaskDto>> GetFilteredTasksAsync(ETaskStatus? status, DateTime? dueDate);
+        Task<TaskDto?> GetTaskByIdAsync(Guid id);
+        Task<TaskDto> AddTaskAsync(CreateTaskDto createTaskDto);
+        Task UpdateTaskAsync(TaskDto taskDto);
+        Task DeleteTaskAsync(Guid id);
+    }
+}
