fab448a3ebe3a85e0b33e16db4626e53d87fe2b3
Thiago Figueira <thi.figueira@hotmail.com>
2025-02-22 22:06:38 -0300

    feat: add dataannotations and mapper dto

---
+++ TaskManagement.Application/Mappers/TaskMapper.cs
@@ -0,0 +1,64 @@
+﻿using TaskManagement.Application.DTOs;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Mappers
+{
+    /// <summary>
+    /// Classe responsável por mapear TaskModel para TaskDto e vice-versa.
+    /// </summary>
+    public static class TaskMapper
+    {
+        /// <summary>
+        /// Converte um TaskModel para TaskDto.
+        /// </summary>
+        public static TaskDto ToDto(this TaskModel task)
+        {
+            return new TaskDto
+            {
+                Id = task.Id,
+                Title = task.Title,
+                Description = task.Description,
+                DueDate = task.DueDate,
+                Status = task.Status
+            };
+        }
+
+        /// <summary>
+        /// Converte um TaskDto para TaskModel.
+        /// </summary>
+        public static TaskModel ToEntity(this TaskDto taskDto)
+        {
+            return new TaskModel
+            {
+                Id = taskDto.Id,
+                Title = taskDto.Title,
+                Description = taskDto.Description,
+                DueDate = taskDto.DueDate.GetValueOrDefault(),
+                Status = taskDto.Status
+            };
+        }
+
+        /// <summary>
+        /// Converte um CreateTaskDto para TaskModel.
+        /// </summary>
+        public static TaskModel ToEntity(this CreateTaskDto createTaskDto)
+        {
+            return new TaskModel
+            {
+                Id = Guid.NewGuid(),
+                Title = createTaskDto.Title,
+                Description = createTaskDto.Description,
+                DueDate = createTaskDto.DueDate.GetValueOrDefault(),
+                Status = createTaskDto.Status
+            };
+        }
+
+        /// <summary>
+        /// Converte uma lista de TaskModel para uma lista de TaskDto.
+        /// </summary>
+        public static IEnumerable<TaskDto> ToDtoList(this IEnumerable<TaskModel> tasks)
+        {
+            return tasks.Select(task => task.ToDto()).ToList();
+        }
+    }
+}
